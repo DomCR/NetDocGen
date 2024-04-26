b8b815aba828784799f502b97d05b94f8378e18d
DomCR <DomCR@users.noreply.github.com>
2024-04-26 11:51:03 +0200

    org

---
+++ src/NetDocGen.Runner/Commands/CommandBase.cs
@@ -0,0 +1,48 @@
+﻿using CSUtilities.Extensions;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace NetDocGen.Runner.Commands
+{
+	public abstract class CommandBase
+	{
+		[Argument(0, Description = "Input assembly file path")]
+		public string Input { get; set; } = string.Empty;
+
+		[Option(ShortName = "x", LongName = "xml", Description = "Input xml documentaiton file path")]
+		public string XmlInput { get; set; } = string.Empty;
+
+		[Option(ShortName = "o", LongName = "output", Description = "Output directory path")]
+		public string Output { get; set; }
+
+		[Option(ShortName = "c", LongName = "clear", Description = "Clear output directory")]
+		public bool ClearOutputDirectory { get; set; } = false;
+
+		protected void validateOptions()
+		{
+			Input.TrowIfNullOrEmpty();
+			if (!File.Exists(Input))
+			{
+				throw new ArgumentException();
+			}
+
+			Output.TrowIfNullOrEmpty();
+
+			if (XmlInput.IsNullOrEmpty())
+			{
+				XmlInput = Path.ChangeExtension(this.Input, "xml");
+			}
+			if (!File.Exists(XmlInput))
+			{
+				throw new ArgumentException();
+			}
+		}
+
+		protected void processOptions()
+		{
+			if(ClearOutputDirectory && Directory.Exists(Output))
+			{
+				Directory.Delete(Output, true);
+			}
+		}
+	}
+}
--- src/NetDocGen.Runner/Commands/MarkdownWikiCommand.cs
+++ src/NetDocGen.Runner/Commands/MarkdownWikiCommand.cs
@@ -1,5 +1,4 @@
-﻿using CSUtilities.Extensions;
-using McMaster.Extensions.CommandLineUtils;
+﻿using McMaster.Extensions.CommandLineUtils;
 using NetDocGen.Services;
 using NetDocGen.Xml;
 using System.Reflection;
@@ -29,49 +28,6 @@ namespace NetDocGen.Runner.Commands
 			}
 
 			this._generator.Generate(doc, this.Output);
-		}
-	}
-
-	public abstract class CommandBase
-	{
-		[Argument(0, Description = "Input assembly file path")]
-		public string Input { get; set; } = string.Empty;
-
-		[Option(ShortName = "x", LongName = "xml", Description = "Input xml documentaiton file path")]
-		public string XmlInput { get; set; } = string.Empty;
-
-		[Option(ShortName = "o", LongName = "output", Description = "Output directory path")]
-		public string Output { get; set; }
-
-		[Option(ShortName = "c", LongName = "clear", Description = "Clear output directory")]
-		public bool ClearOutputDirectory { get; set; } = false;
-
-		protected void validateOptions()
-		{
-			Input.TrowIfNullOrEmpty();
-			if (!File.Exists(Input))
-			{
-				throw new ArgumentException();
-			}
-
-			Output.TrowIfNullOrEmpty();
-
-			if (XmlInput.IsNullOrEmpty())
-			{
-				XmlInput = Path.ChangeExtension(this.Input, "xml");
-			}
-			if (!File.Exists(XmlInput))
-			{
-				throw new ArgumentException();
-			}
-		}
-
-		protected void processOptions()
-		{
-			if(ClearOutputDirectory && Directory.Exists(Output))
-			{
-				Directory.Delete(Output, true);
-			}
 		}
 	}
 }
